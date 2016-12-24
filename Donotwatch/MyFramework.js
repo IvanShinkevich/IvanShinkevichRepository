14c06664b01c5072ceba7a85b62d87549879f817
IvanShinkevich <2014ivanee@gmail.com>
2016-12-24 14:34:00 +0300

    A little bit more order

--- Donotwatch/MyFramework.js
+++ Donotwatch/MyFramework.js
@@ -1,84 +1,57 @@
-var fr={
-	remove: function(el){
-		el.parentNode.removeChild(el);
-	},
-	appendTo: function(el,target){
-		target.insertBefore(el,target.firstChild);
-	},
-	prependTo: function(el,target){
-		target.appendChild(el);
-	},
-	before: function(el,target){
-		target.parentNode.insertBefore(el,target);
-	},
-	after: function(el,target){
-		target.parentNode.insertBefore(el,target.nextSibling);
-	},
-	replace: function(el,target){
-		target.parentNode.replaceChild(el,target);
-	},
-	copyTo: function(el,target){
-		var copy=el.cloneNode(true);
-		target.appendChild(copy);
-	},
-	random: function(a,b){
-		var s=Math.random();
-		s=s*(b-a)+a;
-		return s;
-	},
-	event: function(ev,el,f){
-		if(typeof el.addEventListener=="function"){
-			el.addEventListener(ev,f);
-		}else{
-			el.attachEvent(ev,f);
-		}
-	},
-	unevent: function(ev,el,f){
-		if(typeof el.removeEventListener=="function"){
-			el.removeEventListener(ev,f);
-		}else{
-			el.detachEvent(ev,f);
-		}
-	},
-	dispatch: function(ev,el){
-		if(typeof el.dispatchEvent=='function'){
-		var event=new Event(ev);
-		el.dispatchEvent(event);
-	}else{
-		var event=new Event(ev);
-		el.fireEvent(event);
-	}
-	},
-	ajax: function(m,path,f){
-			var xhr=new XMLHttpRequest();
-			xhr.onreadystatechange=f;
-			xhr.open(m,path,true);
-			xhr.send();
-	},
-	ajget: function(path,f){
-			var xhr=new XMLHttpRequest();
-			xhr.onreadystatechange=f;
-			xhr.open("GET",path,true);
-			xhr.send();
-		},
-	get: {
-		byId: function(el){
-			return document.getElementById(el);
-		},
-		byTagName: function(el){
-			return document.getElementsByTagName(el);
-		},
-		byClassName: function(el){
-			return document.getElementsByClassName(el);
-		},
-		byName: function(el){
-			var tyu=new Array;
-			for (var i = 0; i < document.body.children.length; i++) {
-				if (document.body.children[i].getAttribute('name')==el){
-					tyu.push(document.body.children[i]);
-				}
+var MyFramework={
+		// append:function(el2){
+		// 	 el2.appendChild(el2)
+		// },
+		By:
+		{
+			id:function(id){
+				return document.getElementById(id+'');
+			},
+			// className:function(cl){
+			// 	return document.getElementsByClassName(cl+'');
+			// },
+			tagName:function(tag){
+				return document.getElementsByTagName(tag+'');
 			}
-			return tyu;
+			// },
+			// selector:function(s){
+			// 	return document.getElementBySelector(s+'');
+			// },
+
 		}
-}
+		// remove:function(el){
+		// 	 el.remove(el);
+		// },
+		// appendTo:function(el,target){
+		// 	 target.appendChild(el,target.firstChild)
+		// },
+		// prependTo:function(el,target){
+		// 	 target.appendChild(el);
+		// },
+		// before:function(el,target){
+		// 	 target.parentNode.insertBefore(el,target)
+		// },
+		// after:function(el,target){
+		// 	 target.parentNode.insertBefore(el,target.NextSibling);
+		// }, 
+		// replace:function(el,target){
+		// 	 el.replace(target,el);
+		// },
+		// copyTo:function(el,target){
+		// 	target.appendChild(el.cloneNodes(true));
+		// },
+		// random:function(a,b){
+		//  (Math.random()*(b-a)+a);
+		// },
+		// event:function(el,ev,f){
+		// if(typeof(el.add.eventListener(ev+'',f))=="function")
+		// el.addEventListener(ev+'',f);
+		// else el.attachEvent(ev+'',f);
+		// }
+		// unevent:function(el,ev,f){
+		// if(typeof(el.removeEventListener(ev+'',f))=="function")
+		// el.removeEventListener(ev+'',f)
+		// else el.detachEvent(ev+'',f);
+		// }
+		
 }
